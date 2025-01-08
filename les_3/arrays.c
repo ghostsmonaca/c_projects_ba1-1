00adf0d1fa1964bbd0be8b9756263b6abd10be5d
Kina <kina.broeders@student.uhasselt.be>
2025-01-08 15:22:55 +0100

    Long story short: arrays in c suck

--- les_3/arrays.c
+++ les_3/arrays.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
-int countelements(int array[]){
-
+/*int countelements(int array[]){
+    printf("%d", sizeof(int));
+    printf("lengte array: %d\n", sizeof(array)/sizeof(int));
     int counter = 0;
     for(int i=0; array[i] != '\0'; i++){
         counter++;
     };
     return counter;
+    return 0;
+}*/
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min])
+                min = j;
+        }
+       if (min != i) {
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++){
+        printf("%d ", arr[i]);}
 }
+//https://www.geeksforgeeks.org/c-program-to-sort-an-array-in-ascending-order/
 
-void minimumarray(int array[]){
-
-    int lengte = countelements(array);
-    printf("%d", lengte);
+void minimumarray(int array[], int lengte){
 
     int resul = array[0];
 
@@ -30,8 +48,14 @@ int main(){
     int array1[10] = {1,2,3,4,5,6,7,8,9,10};
     int array2[5] = {4,8,2,6,5};
 
-    minimumarray(array1);
-    minimumarray(array2);
+    int len1 = sizeof(array1) / sizeof(array1[0]);
+    int len2 = sizeof(array2) / sizeof(array2[0]);
+
+    minimumarray(array1, len1);
+    minimumarray(array2, len2);
+
+    selectionSort(array1, len1);
+    selectionSort(array2, len2);
 
     return 0;
 
